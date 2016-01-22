f214b12158b6dd02d2dbc26c89a2b0e87fb0b414
Andreas Gohr <andi@splitbrain.org>
2016-01-22 17:39:24 +0100

    Adjust method signatures to match parent

--- action/export.php
+++ action/export.php
@@ -16,7 +16,7 @@ class action_plugin_nsexport_export extends DokuWiki_Action_Plugin {
 
     var $run = false;
 
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('TPL_ACT_UNKNOWN','BEFORE',  $this, 'nsexport');
         $controller->register_hook('ACTION_ACT_PREPROCESS','BEFORE',  $this, 'act');
     }
